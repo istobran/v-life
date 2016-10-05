fe0fac189a5788bc0556fbfdb63e054b26b221f2
BangZ <i@bangz.me>
2016-10-05 11:59:27 +0800

    added sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <div id="sidebar">
+    <div id="userinfo">
+      <span class="image avatar48"><img :src="user.avatarUrl" alt="" /></span>
+      <h1 id="username">{{ user.username }}</h1>
+      <p id="description" v-if="user.hasDesc">{{ user.description }}</p>
+    </div>
+  </div>
+  <nav id="nav">
+    <ul>
+      <li v-for="item in navList">
+        <a :href="item.linkUrl"><span :class="item.classText">{{ item.text }}</span></a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        avatarUrl: "avatar.jpg",
+        username: "BangZ",
+        hasDesc: true,
+        description: "Front-End Engineer"
+      },
+      navList: [
+        {
+          linkUrl: "#calendar",
+          classText: "icon fa-home",
+          text: "我的日历"
+        }
+      ]
+    };
+  },
+  computed: {},
+  ready() {},
+  attached() {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style lang="css">
+</style>
